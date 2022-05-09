61dd50c3bbe9629da6655a845af25c02e3ac51c6
Everth Paredes <epe0413@gmail.com>
2022-05-09 16:11:33 -0600

    Se agrego el primer Custom Hook

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import useSelectMonedas from '../hooks/useSelectMonedas'
 
 const InputSubmit = styled.input`
     background-color: #9497ff;
@@ -20,8 +21,14 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
+
+  const [ SelectMonedas ] =useSelectMonedas('Elije tu moneda')
+  
+
   return (
     <form>
+        <SelectMonedas/>
+
         <InputSubmit
             type='submit'
             value='Cotizar'
---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Label = styled.label `
+    color: #FFF;
+`
+
+const useSelectMonedas = (label) => {
+  const SelectMonedas = () => (
+      <>
+        <Label>{label}</Label>
+      </>
+  )
+
+  return [SelectMonedas]
+}
+
+export default useSelectMonedas
