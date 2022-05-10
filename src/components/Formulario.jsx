3f0fcb0e06e985ed831a4110213df303af4255cf
Everth Paredes <epe0413@gmail.com>
2022-05-09 21:18:44 -0600

    Conectamos la aplicación a la API de CryptoCompare

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {useEffect} from 'react'
 import styled from '@emotion/styled'
 import useSelectMonedas from '../hooks/useSelectMonedas'
 
@@ -26,11 +26,21 @@ const Formulario = () => {
 
   const [ moneda, SelectMonedas ] =useSelectMonedas('Elije tu moneda', monedas)
   
+  useEffect(()=>{
+    const consultarAPI = async()=>{
+      const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD"
+      const respuesta = await fetch(url)
+      const resultado =await respuesta.json()
+      console.log(resultado.Data)
+
+    }
+    consultarAPI();
+  },[])
 
   return (
     <form>
         <SelectMonedas/>
-        
+
         <InputSubmit
             type='submit'
             value='Cotizar'
